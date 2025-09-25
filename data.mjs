496725d9da4fbf853525e8479401d54874c19e70
lusirahma29 <lusirahmajobs@gmail.com>
2025-09-25 14:54:50 +0700

    tugas 6 javascript

---
+++ data.mjs
@@ -0,0 +1,14 @@
+const users = [
+    { nama: 'Alika', umur: 23, alamat: 'jln. Delima', email: 'alika@gmail.com' },
+    { nama: 'Risma', umur: 26, alamat: 'jln. Jeruk', email: 'risma@gmail.com' },
+    { nama: 'Sahlaa', umur: 10, alamat: 'jln. Mangga', email: 'sahlaa@gmail.com' },
+    { nama: 'Lusi', umur: 20, alamat: 'jln. Kelengkeng', email: 'lusi@gmail.com' },
+    { nama: 'Dayinta', umur: 21, alamat: 'jln. Manggis', email: 'dayinta@gmail.com' },
+    { nama: 'Firly', umur: 25, alamat: 'jln. Pisang', email: 'firly@gmail.com' },
+    { nama: 'Muti', umur: 27, alamat: 'jln. Salak', email: 'muti@gmail.com' },
+    { nama: 'Ardi', umur: 45, alamat: 'jln. Anggur', email: 'ardi@gmail.com' },
+    { nama: 'Yuyun', umur: 40, alamat: 'jln. Mengkudu', email: 'yuyun@gmail.com' },
+    { nama: 'Puspa', umur: 30, alamat: 'jln. Nanas', email: 'puspa@gmail.com' },
+]
+
+export default users;
